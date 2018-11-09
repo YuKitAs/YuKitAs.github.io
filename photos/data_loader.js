3f8638edae944a1236468f484aff0527d07bc7dc
YuKitAs <moncher.merci@gmail.com>
2018-11-09 22:27:36 +0100

    Rename metadata.json to manifest.json

--- photos/data_loader.js
+++ photos/data_loader.js
@@ -1,5 +1,5 @@
 function load() {
-  const url = `https://raw.githubusercontent.com/YuKitAs/YuKitAs.github.io/master/photos/metadata.json`;
+  const url = `https://raw.githubusercontent.com/YuKitAs/YuKitAs.github.io/master/photos/manifest.json`;
 
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
