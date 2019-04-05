9e29e648fa79a3ca58a531eb24fd0618ba79811f
YuKitAs <moncher.merci@gmail.com>
2019-04-06 01:58:51 +0200

    Modify quotes

--- js/initialize.js
+++ js/initialize.js
@@ -1,11 +1,11 @@
 function renderHeader() {
   const greeting = `$ echo "${_greet()}, Sekai!"`;
-  const nav = "<div class=\"nav\"> \
-                <a href=\"/\"><span class=\"nav-item\">Home</span></a> | \
-                <a href=\"/photos\"><span class=\"nav-item\">Photos</span></a> | \
-                <a href=\"/drawings\"><span class=\"nav-item\">Drawings</span></a> | \
-                <a href=\"/about\"><span class=\"nav-item\">About</span></a> \
-              </div>";
+  const nav = `<div class="nav">
+                  <a href="/"><span class="nav-item">Home</span></a> |
+                  <a href="/photos"><span class="nav-item">Photos</span></a> |
+                  <a href="/drawings"><span class="nav-item">Drawings</span></a> |
+                  <a href="/about"><span class="nav-item">About</span></a>
+                </div>`;
   document.body.innerHTML = `<h1>${greeting}</h1>` + `${nav}` + document.body.innerHTML;
 }
 
