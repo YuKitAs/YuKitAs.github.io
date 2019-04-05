a18918a84023b2b3fd841910d9901e7ff7660636
YuKitAs <moncher.merci@gmail.com>
2019-04-06 01:45:00 +0200

    Get rid of the js utility code :(

--- js/initialize.js
+++ js/initialize.js
@@ -1,21 +1,13 @@
-const initialize = {
-  doms: {
-    app: "#app"
-  },
-
-  data: {},
-
-  init (g) {
-    const greeting = `$ echo "${_greet()}, Sekai!"`;
-    const nav = `<div class="nav">
-                  <a href="/"><span class="nav-item">Home</span></a> |
-                  <a href="/photos"><span class="nav-item">Photos</span></a> |
-                  <a href="/drawings"><span class="nav-item">Drawings</span></a> |
-                  <a href="/about"><span class="nav-item">About</span></a>
-                </div>`;
-    g.doms.app.innerHTML = `<h1>${greeting}</h1>` + `${nav}`;
-  }
-};
+function renderHeader() {
+  const greeting = `$ echo "${_greet()}, Sekai!"`;
+  const nav = "<div class=\"nav\"> \
+                <a href=\"/\"><span class=\"nav-item\">Home</span></a> | \
+                <a href=\"/photos\"><span class=\"nav-item\">Photos</span></a> | \
+                <a href=\"/drawings\"><span class=\"nav-item\">Drawings</span></a> | \
+                <a href=\"/about\"><span class=\"nav-item\">About</span></a> \
+              </div>";
+  document.body.innerHTML = `<h1>${greeting}</h1>` + `${nav}` + document.body.innerHTML;
+}
 
 function _greet() {
   let hour = new Date().getHours();
